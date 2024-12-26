e21860a42e58672881696ade6581cd4ea2e2ff0b
Aayush Sharma <121752424+Aayush1301@users.noreply.github.com>
2024-12-26 18:45:07 +0530

    Gemini-clone

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import Sidebar from './components/sidebar/sidebar';
+import Main from './components/main/Main';
+
+function App() {
+  return (
+    <>
+<Sidebar/>
+<Main/>
+    </>
+  );
+}
+
+export default App
